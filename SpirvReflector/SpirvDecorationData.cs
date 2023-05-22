a859607c7ba3b6af1cb70f726f12d35414583458
James Yarwood <syncaidius@googlemail.com>
2023-05-22 06:02:19 +0100

    Added indexer for accessing decoration parameters
    Added missing summaries to SpirvDecorationData
    Bumped version to 0.1.1

--- SpirvReflector/SpirvDecorationData.cs
+++ SpirvReflector/SpirvDecorationData.cs
@@ -10,24 +10,47 @@ namespace SpirvReflector
     {
         Dictionary<SpirvDecoration, List<object>> _decorations = new Dictionary<SpirvDecoration, List<object>>();
 
+        /// <summary>
+        /// Adds a <see cref="SpirvDecoration"/> to the current <see cref="SpirvDecorationData"/> instance. The same <see cref="SpirvDecoration"/> cannot be added twice.
+        /// </summary>
+        /// <param name="decoration">The <see cref="SpirvDecoration"/>.</param>
+        /// <param name="values">The list of decoration parameters.</param>
+        /// <exception cref="InvalidOperationException"></exception>
         internal void AddDecoration(SpirvDecoration decoration, List<object> values)
         {
             if (_decorations.ContainsKey(decoration))
                 throw new InvalidOperationException($"The same decoration ({decoration}) cannot be added twice.");
 
-            _decorations.Add(decoration, values);
+            _decorations.Add(decoration, values ?? new List<object>());
         }
 
+        /// <summary>
+        /// Checks whether the current <see cref="SpirvDecorationData"/> instance contains a specific <see cref="SpirvDecoration"/>.
+        /// </summary>
+        /// <param name="decoration">The <see cref="SpirvDecoration"/>.</param>
+        /// <returns></returns>
         public bool HasDecoration(SpirvDecoration decoration)
         {
             return _decorations.ContainsKey(decoration);
         }
 
-        public IReadOnlyList<object> GetDecorationValues(SpirvDecoration decoration)
-        {
-            return _decorations[decoration];
-        }
-
+        /// <summary>
+        /// Gets a list of <see cref="SpirvDecoration"/> contained in the current <see cref="SpirvDecorationData"/> instance.
+        /// </summary>
         public IEnumerable<SpirvDecoration> Keys => _decorations.Keys;
+
+        /// <summary>
+        /// Retrieves the list of object parameters for a specific <see cref="SpirvDecoration"/>. Returns null if the key does not exist.
+        /// </summary>
+        /// <param name="key">The <see cref="SpirvDecoration"/> for which to retrieve object parameters.</param>
+        /// <returns></returns>
+        public IReadOnlyList<object> this[SpirvDecoration key]
+        {
+            get
+            {
+                _decorations.TryGetValue(key, out List<object> values);
+                return values;
+            }
+        }
     }
 }
